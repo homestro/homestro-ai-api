9737220d90ccb0837e743d15fd10aed1a165a401
homestro <Homestro@web.de>
2026-09-25 23:26:31 +0200

    Fix catalog parser string quoting

--- server.js
+++ server.js
@@ -574,9 +574,9 @@ async function catalogSearch(keyword){
 
     const blocks=[];
     const patterns=[
-      new RegExp('<li[^>]*class=["\\']?[^"\\']*b_algo[^"\\']*["\\']?[^>]*>[\\s\\S]*?<\\/li>','gi'),
-      new RegExp('<div[^>]*class=["\\']?[^"\\']*MjjYud[^"\\']*["\\']?[^>]*>[\\s\\S]{0,18000}?<\\/div>','gi'),
-      new RegExp('<div[^>]*class=["\\']?[^"\\']*result[^"\\']*["\\']?[^>]*>[\\s\\S]{0,12000}?<\\/div>','gi')
+      /<li[^>]*class=["']?[^"']*b_algo[^"']*["']?[^>]*>[\\s\\S]*?<\\/li>/gi,
+      /<div[^>]*class=["']?[^"']*MjjYud[^"']*["']?[^>]*>[\\s\\S]{0,18000}?<\\/div>/gi,
+      /<div[^>]*class=["']?[^"']*result[^"']*["']?[^>]*>[\\s\\S]{0,12000}?<\\/div>/gi
     ];
     for(const re of patterns){let bm;while((bm=re.exec(normalized))&&blocks.length<400)blocks.push(bm[0]);}
     for(const block of blocks)addSearchBlock(block,'search-engine');
